649fd241ab324668b024c9551df401c597266117
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-07 19:44:37 +0300

    add AppConfig

--- app/config.py
+++ app/config.py
@@ -1,8 +1,15 @@
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class AppConfig(BaseModel):
+    PORT: int = 80
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_nested_delimiter="__")
+
+    APP: AppConfig = AppConfig()
 
 
 settings = Settings()
