db586b694909ac7644c3afbcc59b3a78d2d58c70
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-07 20:21:03 +0300

    add DatabaseConfig

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -6,10 +6,26 @@ class AppConfig(BaseModel):
     PORT: int = 80
 
 
+class DatabaseConfig(BaseModel):
+    HOST: str = "localhost"
+    PORT: int = "5432"
+    USER: str = "user"
+    PASS: SecretStr = SecretStr("password")
+    NAME: str = "my_db"
+
+    @property
+    def URL(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.USER}:{self.PASS}"
+            f"@{self.HOST}:{self.PORT}/{self.NAME}"
+        )
+
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_nested_delimiter="__")
 
     APP: AppConfig = AppConfig()
+    DB: DatabaseConfig = DatabaseConfig()
 
 
 settings = Settings()
