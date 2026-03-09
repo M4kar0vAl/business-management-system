3fb54a92955f1023749bd05df9335bfd34a4fc48
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-09 18:23:18 +0300

    add user manager dependency

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -4,11 +4,13 @@ from fastapi import Depends
 from fastapi_users.authentication.strategy.db import DatabaseStrategy
 
 from app.auth.models import AccessToken, User
+from app.auth.user_manager import UserManager
 from app.config import settings
 from app.dependencies import get_session
 
 if TYPE_CHECKING:
     from fastapi_users.authentication.strategy.db import AccessTokenDatabase
+    from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
@@ -28,3 +30,9 @@ def get_database_strategy(
     return DatabaseStrategy(
         access_token_db, lifetime_seconds=settings.ACCESS_TOKEN.LIFETIME_SECONDS
     )
+
+
+async def get_user_manager(
+    user_db: Annotated[SQLAlchemyUserDatabase, Depends(get_user_db)],
+):
+    yield UserManager(user_db)
