3bf87dcaef4e624021154491c1464a352695c60a
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-09 16:19:10 +0300

    add get_db method in User model

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,11 +1,15 @@
 from enum import StrEnum
+from typing import TYPE_CHECKING
 
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.auth.types import UserIdType
 from app.mixins import IdIntPkMixin
 from app.models import Base
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class Role(StrEnum):
@@ -18,3 +22,7 @@ class User(SQLAlchemyBaseUserTable[UserIdType], IdIntPkMixin, Base):
     __tablename__ = "users"
 
     role: Mapped[Role] = mapped_column(default=Role.USER, server_default=Role.USER.name)
+
+    @classmethod
+    def get_db(cls, session: AsyncSession) -> SQLAlchemyUserDatabase:
+        return SQLAlchemyUserDatabase(session, cls)
