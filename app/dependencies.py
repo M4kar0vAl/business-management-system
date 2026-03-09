33df79ab78dbb785ea923c68fa9363b0d6d9b7a1
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-09 17:33:30 +0300

    update imports

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,12 +1,12 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy.ext.asyncio import AsyncSession
-
 from app.database import Session
 
 if TYPE_CHECKING:
     from collections.abc import AsyncGenerator
     from typing import Any
+
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def get_session() -> AsyncGenerator[AsyncSession, Any]:
