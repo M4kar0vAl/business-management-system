ec35af3f04ac8cc2a79f000e6a9ea96f6e23f8cd
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-09 20:09:27 +0300

    add FastAPIUsers instance

---
+++ app/auth/fastapi_users.py
@@ -0,0 +1,11 @@
+from fastapi_users import FastAPIUsers
+
+from .backend import authentication_backend
+from .dependencies import get_user_manager
+from .models import User
+from .types import UserIdType
+
+fastapi_users = FastAPIUsers[User, UserIdType](
+    get_user_manager,
+    [authentication_backend],
+)
