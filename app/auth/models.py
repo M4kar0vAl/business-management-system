68d5fe7ba5f65d3e9bec1c8b94486891fee238c7
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-09 16:43:15 +0300

    add access token model

--- app/auth/models.py
+++ app/auth/models.py
@@ -2,6 +2,11 @@ from enum import StrEnum
 from typing import TYPE_CHECKING
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyAccessTokenDatabase,
+    SQLAlchemyBaseAccessTokenTable,
+)
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.auth.types import UserIdType
@@ -26,3 +31,15 @@ class User(SQLAlchemyBaseUserTable[UserIdType], IdIntPkMixin, Base):
     @classmethod
     def get_db(cls, session: AsyncSession) -> SQLAlchemyUserDatabase:
         return SQLAlchemyUserDatabase(session, cls)
+
+
+class AccessToken(SQLAlchemyBaseAccessTokenTable[UserIdType], Base):
+    __tablename__ = "access_tokens"
+
+    user_id: Mapped[UserIdType] = mapped_column(
+        ForeignKey("users.id", ondelete="cascade"), nullable=False
+    )
+
+    @classmethod
+    def get_db(cls, session: AsyncSession) -> SQLAlchemyAccessTokenDatabase:
+        return SQLAlchemyAccessTokenDatabase(session, cls)
