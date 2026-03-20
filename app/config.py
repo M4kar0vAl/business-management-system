b72bfae3638948249f36c9481b0b15ddf8b487d8
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-20 20:50:31 +0300

    add naming convention setting for db

--- app/config.py
+++ app/config.py
@@ -19,6 +19,13 @@ class DatabaseConfig(BaseModel):
     USER: str = "user"
     PASS: SecretStr = SecretStr("password")
     NAME: str = "my_db"
+    NAMING_CONVENTION: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
     @property
     def URL(self) -> SQLA_URL:
