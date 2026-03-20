916719c61e3af8664bc3625f5a4533d2f7775c07
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-20 16:50:40 +0300

    create UnitOfWork

---
+++ app/uow.py
@@ -0,0 +1,30 @@
+from contextlib import asynccontextmanager
+
+from app.database import Session
+
+
+class UnitOfWork:
+    def __init__(self):
+        self.session = Session()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
+
+    async def close(self):
+        await self.session.close()
+
+
+@asynccontextmanager
+async def unit_of_work():
+    uow = UnitOfWork()
+    try:
+        yield uow
+        await uow.commit()
+    except Exception:
+        await uow.rollback()
+        raise
+    finally:
+        await uow.close()
