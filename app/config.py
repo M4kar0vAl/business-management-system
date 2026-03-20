cc7193dead4643561ae0d28823f50136af7876e6
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-20 14:23:45 +0300

    add ADMIN_PANEL_SECRET_KEY setting

--- app/config.py
+++ app/config.py
@@ -36,6 +36,7 @@ class AuthenticationConfig(BaseModel):
     ACCESS_TOKEN_LIFETIME_SECONDS: int = 604800  # 7 days
     RESET_PASSWORD_TOKEN_SECRET: SecretStr = "secret"
     VERIFICATION_TOKEN_SECRET: SecretStr = "secret"
+    ADMIN_PANEL_SECRET_KEY: SecretStr = "secret"
 
 
 class PasswordConfig(BaseModel):
