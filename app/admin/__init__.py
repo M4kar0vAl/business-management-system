32a7218047ff17537e7f61fd551a25bb9eccdba4
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-20 14:28:15 +0300

    add authentication for admin panel

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -5,13 +5,15 @@ __all__ = [
 from fastapi import FastAPI
 from sqladmin import Admin
 
-from app.auth import admin as auth_admin
 from app.auth.admin import views as auth_admin
+from app.auth.admin.authentication import admin_authentication_backend
 from app.database import Session
 
 
 def register_admin_views(app: FastAPI):
-    admin = Admin(app, session_maker=Session)
+    admin = Admin(
+        app, session_maker=Session, authentication_backend=admin_authentication_backend
+    )
 
     # auth
     admin.add_view(auth_admin.AccessTokenAdmin)
