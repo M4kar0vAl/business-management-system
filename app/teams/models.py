076952a01a3d8f8177a70c82e4294ec96c0aa65f
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-20 20:51:23 +0300

    add team_id column for User model

--- app/teams/models.py
+++ app/teams/models.py
@@ -1,11 +1,17 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String
 from sqlalchemy.orm import (
     Mapped,
     mapped_column,
+    relationship,
 )
 
 from app.mixins import IdIntPkMixin
 from app.models import Base
+
+if TYPE_CHECKING:
+    from app.auth.models import User
 
 
 class Team(IdIntPkMixin, Base):
@@ -13,3 +19,5 @@ class Team(IdIntPkMixin, Base):
 
     name: Mapped[str] = mapped_column(String(256), unique=True)
     description: Mapped[str] = mapped_column(String(512), default="", server_default="")
+
+    users: Mapped[list[User]] = relationship(back_populates="team")
