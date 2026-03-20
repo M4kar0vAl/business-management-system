c3e63aa89508139d6e3457d3a65b6885bea07965
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-20 16:51:12 +0300

    add get_uow dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 
 from app.database import Session
+from app.uow import UnitOfWork, unit_of_work
 
 if TYPE_CHECKING:
     from collections.abc import AsyncGenerator
@@ -12,3 +13,8 @@ if TYPE_CHECKING:
 async def get_session() -> AsyncGenerator[AsyncSession, Any]:
     async with Session() as session:
         yield session
+
+
+async def get_uow() -> AsyncGenerator[UnitOfWork, Any]:
+    async with unit_of_work() as uow:
+        yield uow
