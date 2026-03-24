6ad273cadfd2cffc5ccc189f6d528d5d2ff8b229
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-24 19:55:05 +0300

    add docstring to exception_handler_factory

--- app/exception_handlers.py
+++ app/exception_handlers.py
@@ -5,6 +5,14 @@ from fastapi.responses import JSONResponse
 def exception_handler_factory(
     app: FastAPI, exc_type: type[Exception], status_code: int
 ):
+    """
+    Exception handler factory.
+
+    :param app: fastapi app to register exception handlers for
+    :param exc_type: exception class to register handler for
+    :param status_code: http status code to return in response
+    :return: exception handler
+    """
 
     @app.exception_handler(exc_type)
     async def handler(_: Request, exc: Exception):
