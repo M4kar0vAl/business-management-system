7c8a0ad22d2151242ce9e245dc60798da32cf1fd
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-24 19:52:42 +0300

    refactor teams/exception_handlers.py

--- app/teams/exception_handlers.py
+++ app/teams/exception_handlers.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, Request, status
-from starlette.responses import JSONResponse
+from fastapi import FastAPI, status
 
+from app.exception_handlers import exception_handler_factory
 from app.teams.exceptions import (
     TeamAlreadyExistsError,
     TeamDoesNotExistError,
@@ -8,35 +8,14 @@ from app.teams.exceptions import (
     UserNotInTeamError,
 )
 
+exception_status_mapping = {
+    TeamDoesNotExistError: status.HTTP_404_NOT_FOUND,
+    TeamAlreadyExistsError: status.HTTP_400_BAD_REQUEST,
+    UserAlreadyInTeamError: status.HTTP_400_BAD_REQUEST,
+    UserNotInTeamError: status.HTTP_400_BAD_REQUEST,
+}
+
 
 def register_exception_handlers(app: FastAPI):
-
-    @app.exception_handler(TeamDoesNotExistError)
-    async def team_does_not_exist_exception_handler(
-        _: Request, exc: TeamDoesNotExistError
-    ):
-        return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND, content={"detail": exc.message}
-        )
-
-    @app.exception_handler(TeamAlreadyExistsError)
-    async def team_already_exists_exception_handler(
-        _: Request, exc: TeamAlreadyExistsError
-    ):
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST, content={"detail": exc.message}
-        )
-
-    @app.exception_handler(UserAlreadyInTeamError)
-    async def user_already_in_team_exception_handler(
-        _: Request, exc: UserAlreadyInTeamError
-    ):
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST, content={"detail": exc.message}
-        )
-
-    @app.exception_handler(UserNotInTeamError)
-    async def user_not_in_team_exception_handler(_: Request, exc: UserNotInTeamError):
-        return JSONResponse(
-            status_code=status.HTTP_400_BAD_REQUEST, content={"detail": exc.message}
-        )
+    for exc_type, status_code in exception_status_mapping.items():
+        exception_handler_factory(app, exc_type, status_code)
