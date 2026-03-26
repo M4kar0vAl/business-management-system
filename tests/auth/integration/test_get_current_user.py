3633b53ce3c6ca5e5f42fa65ba97206689bcb75b
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-26 16:59:35 +0300

    add integration tests for get current user endpoint

---
+++ tests/auth/integration/test_get_current_user.py
@@ -0,0 +1,44 @@
+from fastapi import status
+
+from app.auth.schemas import UserCreate
+from app.main import app
+
+
+class TestGetCurrentUser:
+    url = app.url_path_for("users:current_user")
+
+    async def test_get_current_user(
+        self, async_client, create_user, get_authorization_header
+    ):
+        user_create = UserCreate(email="user@example.com", password="Pass!234")
+        user, token = await create_user(user_create, authenticate=True)
+
+        response = await async_client.get(
+            self.url, headers=get_authorization_header(token)
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        data = response.json()
+
+        for field, value in data.items():
+            assert getattr(user, field) == value
+
+    async def test_get_current_user_unauthenticated(self, async_client):
+        response = await async_client.get(self.url)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_get_current_user_inactive(
+        self, async_client, create_user, get_authorization_header
+    ):
+        user_create = UserCreate(
+            email="user@example.com", password="Pass!234", is_active=False
+        )
+        _, token = await create_user(user_create, authenticate=True)
+
+        response = await async_client.get(
+            self.url, headers=get_authorization_header(token)
+        )
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
