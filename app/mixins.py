a5216fa4c119fc43ea7ef45176217e244dc9df35
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-27 19:13:17 +0300

    add CreatedAtMixin

--- app/mixins.py
+++ app/mixins.py
@@ -1,5 +1,14 @@
+from datetime import UTC, datetime
+
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
 
 
 class IdIntPkMixin:
     id: Mapped[int] = mapped_column(primary_key=True)
+
+
+class CreatedAtMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.now(UTC), server_default=func.now()
+    )
