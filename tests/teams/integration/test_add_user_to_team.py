4450e47ec75e793f19d618ac65ac6611823775b6
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-27 15:31:38 +0300

    add integration tests for add user to team endpoint

---
+++ tests/teams/integration/test_add_user_to_team.py
@@ -0,0 +1,126 @@
+import pytest
+from fastapi import status
+
+from app.auth.models import Role
+from app.auth.schemas import UserCreate
+from app.auth.types import UserIdType
+from app.teams.routers import ADD_USER_TO_TEAM_ROUTE_NAME
+from app.teams.schemas import TeamCreate
+from tests.mixins import GetUrlMixin
+
+
+@pytest.mark.integration
+class TestAddUserToTeam(GetUrlMixin):
+    url_name = ADD_USER_TO_TEAM_ROUTE_NAME
+
+    def get_url(self, team_id: int, user_id: UserIdType):
+        return super().get_url(team_id=team_id, user_id=user_id)
+
+    async def test_add_user_to_team(
+        self, async_client, create_user, create_team, get_authorization_header, user_db
+    ):
+        user, token = await create_user(
+            UserCreate(email="user@example.com", password="Pass!234", role=Role.ADMIN),
+            authenticate=True,
+        )
+        team = await create_team(TeamCreate(name="team"))
+
+        response = await async_client.post(
+            self.get_url(team.id, user.id), headers=get_authorization_header(token)
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        updated_user = await user_db.get(user.id)
+        assert updated_user is not None
+        assert updated_user.team_id == team.id
+
+    async def test_add_user_to_team_unauthenticated(
+        self, async_client, create_user, create_team
+    ):
+        user, _ = await create_user(
+            UserCreate(email="user@example.com", password="Pass!234", role=Role.ADMIN)
+        )
+        team = await create_team(TeamCreate(name="team"))
+
+        response = await async_client.post(self.get_url(team.id, user.id))
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_add_user_to_team_inactive(
+        self, async_client, create_user, create_team, get_authorization_header
+    ):
+        user, token = await create_user(
+            UserCreate(
+                email="user@example.com",
+                password="Pass!234",
+                role=Role.ADMIN,
+                is_active=False,
+            ),
+            authenticate=True,
+        )
+        team = await create_team(TeamCreate(name="team"))
+
+        response = await async_client.post(
+            self.get_url(team.id, user.id), headers=get_authorization_header(token)
+        )
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    async def test_add_user_to_team_not_an_admin(
+        self, async_client, create_user, create_team, get_authorization_header
+    ):
+        user, token = await create_user(
+            UserCreate(email="user@example.com", password="Pass!234"), authenticate=True
+        )
+        team = await create_team(TeamCreate(name="team"))
+
+        response = await async_client.post(
+            self.get_url(team.id, user.id), headers=get_authorization_header(token)
+        )
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    async def test_add_user_to_team_team_does_not_exist(
+        self, async_client, create_user, get_authorization_header
+    ):
+        user, token = await create_user(
+            UserCreate(email="user@example.com", password="Pass!234", role=Role.ADMIN),
+            authenticate=True,
+        )
+
+        response = await async_client.post(
+            self.get_url(0, user.id), headers=get_authorization_header(token)
+        )
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    async def test_add_user_to_team_user_does_not_exist(
+        self, async_client, create_user, create_team, get_authorization_header
+    ):
+        _, token = await create_user(
+            UserCreate(email="user@example.com", password="Pass!234", role=Role.ADMIN),
+            authenticate=True,
+        )
+        team = await create_team(TeamCreate(name="team"))
+
+        response = await async_client.post(
+            self.get_url(team.id, 0), headers=get_authorization_header(token)
+        )
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    async def test_add_user_to_team_user_already_in_team(
+        self, async_client, create_user, create_team, get_authorization_header
+    ):
+        user, token = await create_user(
+            UserCreate(email="user@example.com", password="Pass!234", role=Role.ADMIN),
+            authenticate=True,
+        )
+        team = await create_team(TeamCreate(name="team"), members=[user])
+
+        response = await async_client.post(
+            self.get_url(team.id, user.id), headers=get_authorization_header(token)
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
