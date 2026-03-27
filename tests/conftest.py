665a110e869634d5f0b16633f3fd3591ef40f680
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-27 13:35:22 +0300

    move auth fixtures into global conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import logging
 
 import pytest
+from fastapi.security import OAuth2PasswordRequestForm
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy import URL, NullPool
 from sqlalchemy.ext.asyncio import async_sessionmaker
 
-from app.auth.models import User
+from app.auth.backend import authentication_backend
+from app.auth.models import AccessToken, User
 from app.auth.schemas import UserCreate
 from app.auth.user_manager import UserManager
 from app.config import DatabaseConfig, Settings
@@ -119,8 +121,30 @@ async def user_db(session):
 
 
 @pytest.fixture
+async def access_token_db(session):
+    return AccessToken.get_db(session)
+
+
+@pytest.fixture
 async def user_manager(user_db):
     return UserManager(user_db)
+
+
+@pytest.fixture
+async def authenticate_user(access_token_db, user_manager):
+    async def _authenticate_user(email, password):
+        user = await user_manager.authenticate(
+            OAuth2PasswordRequestForm(username=email, password=password)
+        )
+
+        assert user is not None, f"Could not authenticate user {email}"
+
+        strategy = authentication_backend.get_strategy(access_token_db)
+        token = await strategy.write_token(user)
+
+        return user, token
+
+    return _authenticate_user
 
 
 @pytest.fixture
@@ -136,3 +160,14 @@ async def create_user(user_manager, authenticate_user):
         return db_user, token
 
     return _create_user
+
+
+@pytest.fixture(scope="session")
+def get_authorization_header():
+
+    def _get_authorization_header(token):
+        return {
+            "Authorization": f"Bearer {token}",
+        }
+
+    return _get_authorization_header
