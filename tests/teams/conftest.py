5b49966d00e1a6fd1131696578792b1fb92b7d18
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-27 14:31:04 +0300

    add members param to create_team fixture

--- tests/teams/conftest.py
+++ tests/teams/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+from app.auth.models import User
 from app.teams.repositories import TeamRepository
 from app.teams.schemas import TeamCreate
 from app.teams.services import TeamService
@@ -16,10 +17,15 @@ async def team_service(uow, user_manager):
 
 
 @pytest.fixture
-async def create_team(team_service):
+async def create_team(team_service, team_repository):
 
-    async def _create_team(team_create: TeamCreate):
+    async def _create_team(team_create: TeamCreate, members: list[User] | None = None):
         team = await team_service.create_team(team_create)
+        members = members or []
+
+        for member in members:
+            await team_repository.assign_user_to_team(team, member)
+
         await team_service.uow.flush()
         return team
 
