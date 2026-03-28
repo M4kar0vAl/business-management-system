1b780fd2570e72456ba8f7cad13ab962a313a0a6
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-28 14:35:36 +0300

    fix mutable default value on created_at field

--- app/mixins.py
+++ app/mixins.py
@@ -10,5 +10,5 @@ class IdIntPkMixin:
 
 class CreatedAtMixin:
     created_at: Mapped[datetime] = mapped_column(
-        default=datetime.now(UTC), server_default=func.now()
+        default_factory=lambda: datetime.now(UTC), server_default=func.now()
     )
