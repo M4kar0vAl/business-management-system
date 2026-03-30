143c4d5c0bbe90479dc0ec41c75bd45bf509a387
Alexey Makarov <alex.makarov2290@gmail.com>
2026-03-30 14:55:01 +0300

    register exception handler for UserIsNotTaskAssigneeError

--- app/tasks/exception_handlers.py
+++ app/tasks/exception_handlers.py
@@ -5,11 +5,13 @@ from app.tasks.exceptions import (
     CommentDoesNotExistError,
     TaskAlreadyAssignedError,
     TaskDoesNotExistError,
+    UserIsNotTaskAssigneeError,
 )
 
 exception_status_mapping = {
     TaskDoesNotExistError: status.HTTP_404_NOT_FOUND,
     TaskAlreadyAssignedError: status.HTTP_400_BAD_REQUEST,
+    UserIsNotTaskAssigneeError: status.HTTP_400_BAD_REQUEST,
     CommentDoesNotExistError: status.HTTP_404_NOT_FOUND,
     CommentDoesNotBelongToUser: status.HTTP_400_BAD_REQUEST,
 }
