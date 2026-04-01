b87546b31e27c5ef7fa962286a722713aee648b3
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-01 16:32:21 +0300

    add integration test for remove user from team endpoint

--- tests/teams/integration/test_remove_user_from_team.py
+++ tests/teams/integration/test_remove_user_from_team.py
@@ -1,8 +1,11 @@
+from datetime import UTC, datetime, timedelta
+
 from fastapi import status
 
 from app.auth.models import Role
 from app.auth.schemas import UserCreate
 from app.auth.types import UserIdType
+from app.tasks.schemas import TaskCreate
 from app.teams.routers import REMOVE_USER_FROM_TEAM_ROUTE_NAME
 from app.teams.schemas import TeamCreate
 from tests.mixins import GetUrlMixin
@@ -106,3 +109,40 @@ class TestRemoveUserFromTeam(GetUrlMixin):
         )
 
         assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    async def test_remove_user_from_team_unassigns_from_team_tasks(
+        self,
+        async_client,
+        create_user,
+        create_team,
+        create_task,
+        user_db,
+        task_repository,
+        get_authorization_header,
+    ):
+        user, token = await create_user(
+            UserCreate(email="user@example.com", password="Pass!234", role=Role.ADMIN),
+            authenticate=True,
+        )
+        team = await create_team(TeamCreate(name="team"), members=[user])
+        task_assigned = await create_task(
+            TaskCreate(
+                description="asfas",
+                deadline=datetime.now(UTC) + timedelta(days=1),
+                team_id=team.id,
+            ),
+            author=user,
+            assignee=user,
+        )
+
+        response = await async_client.delete(
+            self.get_url(user.id), headers=get_authorization_header(token)
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        updated_user = await user_db.get(user.id)
+        updated_task = await task_repository.get_by_id(task_assigned.id)
+        assert updated_user is not None
+        assert updated_user.team_id is None
+        assert updated_task.assignee_id is None
