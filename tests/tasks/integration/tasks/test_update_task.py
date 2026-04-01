e6e3ce43904bac50bb466495dfa2bf35bc5e68d3
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-01 16:56:26 +0300

    add integration test for update_task route

--- tests/tasks/integration/tasks/test_update_task.py
+++ tests/tasks/integration/tasks/test_update_task.py
@@ -134,6 +134,40 @@ class TestUpdateTask(GetUrlMixin):
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
+    async def test_update_task_not_an_author(
+        self,
+        async_client,
+        create_user,
+        create_team,
+        create_task,
+        get_authorization_header,
+    ):
+        user, token = await create_user(
+            UserCreate(
+                email="user@example.com", password="Pass!234", role=Role.MANAGER
+            ),
+            authenticate=True,
+        )
+        another_user, _ = await create_user(
+            UserCreate(email="user1@example.com", password="Pass!234"),
+        )
+        team = await create_team(TeamCreate(name="team"), members=[user, another_user])
+        task = await create_task(
+            TaskCreate(
+                description="asfjsa", deadline=self._some_deadline, team_id=team.id
+            ),
+            author=another_user,
+        )
+        update_dict = TaskUpdate(description="edited").model_dump(exclude_unset=True)
+
+        response = await async_client.patch(
+            self.get_url(task.id),
+            json=update_dict,
+            headers=get_authorization_header(token),
+        )
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
     async def test_update_task_not_exists(
         self, async_client, create_user, get_authorization_header
     ):
