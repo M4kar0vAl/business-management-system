4c4644ca4fe4472df66d43b5193ff3da9b783784
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-07 20:50:15 +0300

    add homepage route

--- app/routers/web_router.py
+++ app/routers/web_router.py
@@ -1,3 +1,22 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Request
+from fastapi.responses import HTMLResponse
+
+from app.auth.fastapi_users_instance import current_active_user_or_none
+from app.auth.models import User
+from app.templates import templates
 
 router = APIRouter()
+
+
+@router.get("/", name="home", response_class=HTMLResponse)
+async def index(
+    current_user: Annotated[User, Depends(current_active_user_or_none)],
+    request: Request,
+):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"current_user": current_user, "title": "Home"},
+    )
