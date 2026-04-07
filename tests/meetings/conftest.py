ca4d514a71b4fd67127fefb2dbde67b37fe3c1e9
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-07 15:16:13 +0300

    add participants parameter to create_meeting fixture

--- tests/meetings/conftest.py
+++ tests/meetings/conftest.py
@@ -17,10 +17,17 @@ async def meeting_service(uow, user_manager):
 
 
 @pytest.fixture
-async def create_meeting(meeting_service):
-    async def _create_meeting(meeting: MeetingCreate, user: User):
+async def create_meeting(meeting_service, meeting_repository):
+    async def _create_meeting(
+        meeting: MeetingCreate, user: User, participants: list[User] | None = None
+    ):
         db_meeting = await meeting_service.create_meeting(meeting, user)
+        participants = participants or []
         await meeting_service.uow.flush()
+
+        for participant in participants:
+            await meeting_repository.add_participant(db_meeting, participant)
+
         return db_meeting
 
     return _create_meeting
