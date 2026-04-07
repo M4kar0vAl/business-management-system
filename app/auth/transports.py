0b56c6ad474bf8cebf2dbd02718f25ecbca13e3d
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-07 20:29:53 +0300

    fix tokenUrl for bearer transport

--- app/auth/transports.py
+++ app/auth/transports.py
@@ -2,7 +2,7 @@ from fastapi_users.authentication import BearerTransport, CookieTransport
 
 from app.config import settings
 
-bearer_transport = BearerTransport(tokenUrl="auth/bearer/login")
+bearer_transport = BearerTransport(tokenUrl="/api/auth/bearer/login")
 cookie_transport = CookieTransport(
     cookie_name=settings.AUTHENTICATION.COOKIE.NAME,
     cookie_max_age=settings.AUTHENTICATION.COOKIE.MAX_AGE,
