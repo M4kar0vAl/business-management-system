100c813119bdd0a7c7ad5d62a23392652390e98f
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-08 17:46:05 +0300

    create unauthenticated_user_or_redirect_home dependency

--- app/auth/fastapi_users_instance.py
+++ app/auth/fastapi_users_instance.py
@@ -1,9 +1,9 @@
 from typing import Annotated
 
-from fastapi import Depends
+from fastapi import Depends, Request
 from fastapi_users import FastAPIUsers
 
-from ..http_exceptions import ForbiddenError
+from ..http_exceptions import ForbiddenError, RedirectException
 from .backend import auth_bearer_db_backend, auth_cookie_db_backend
 from .dependencies import get_user_manager
 from .models import Role, User
@@ -24,6 +24,23 @@ current_active_user_or_none = fastapi_users_instance.current_user(
 current_active_superuser = fastapi_users_instance.current_user(
     active=True, superuser=True
 )
+
+
+async def unauthenticated_user_or_redirect_home(
+    user: Annotated[User | None, Depends(current_active_user_or_none)], request: Request
+):
+    """
+    Dependency to ensure that current user is unauthenticated. Otherwise, redirects to home page.
+
+    :param user: user performing the action or None if there is no authenticated user
+    :param request: fastapi request object
+    :return: None
+    :raises RedirectException: if the user is authenticated. Redirects to home page
+    """
+    if user is None:
+        return
+
+    raise RedirectException(request.url_for("home"))
 
 
 async def get_current_admin(
