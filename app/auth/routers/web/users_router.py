97b191f50da3445c62098ee10c7456d632745585
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-08 19:39:48 +0300

    create users_list_page route

--- app/auth/routers/web/users_router.py
+++ app/auth/routers/web/users_router.py
@@ -5,16 +5,19 @@ from fastapi.responses import HTMLResponse
 from fastapi_users import exceptions
 
 from app.auth.dependencies import get_user_manager
-from app.auth.fastapi_users_instance import current_active_user
+from app.auth.fastapi_users_instance import current_active_user, get_current_admin
 from app.auth.models import User
 from app.auth.schemas import UserUpdate
+from app.auth.services import list_users
 from app.auth.user_manager import UserManager
+from app.dependencies import SessionDep
 from app.templates import templates
 
 router = APIRouter(prefix="/users")
 
 PROFILE_PAGE_ROUTE_NAME = "profile"
 PROFILE_UPDATE_ROUTE_NAME = "profile:update"
+USER_LIST_PAGE_ROUTE_NAME = "users:list_page"
 
 
 @router.get("/me", response_class=HTMLResponse, name=PROFILE_PAGE_ROUTE_NAME)
@@ -50,4 +53,19 @@ async def update_profile(
         request=request,
         name="users/profile.html",
         context={"title": "Profile", "current_user": updated_user, "error": error},
+    )
+
+
+@router.get("/", response_class=HTMLResponse, name=USER_LIST_PAGE_ROUTE_NAME)
+async def users_list_page(
+    session: SessionDep,
+    user: Annotated[User, Depends(get_current_admin)],
+    request: Request,
+):
+    users = await list_users(session)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="users/list.html",
+        context={"title": "Profile", "current_user": user, "users": users},
     )
