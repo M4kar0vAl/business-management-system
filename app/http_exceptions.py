0023ad7567bf3cbd60cdb2100f3f27fabb7bc4ee
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-08 17:40:26 +0300

    create RedirectException http exception

--- app/http_exceptions.py
+++ app/http_exceptions.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from fastapi import HTTPException, status
+from starlette.datastructures import URL
 
 
 class ForbiddenError(HTTPException):
@@ -12,3 +13,14 @@ class ForbiddenError(HTTPException):
         super().__init__(
             status_code=status.HTTP_403_FORBIDDEN, detail=detail, headers=headers
         )
+
+
+class RedirectException(HTTPException):
+    def __init__(
+        self,
+        url: str | URL,
+        status_code: int = status.HTTP_303_SEE_OTHER,
+        headers: dict[str, str] | None = None,
+    ):
+        self.url = url
+        super().__init__(status_code=status_code, detail="Redirect", headers=headers)
