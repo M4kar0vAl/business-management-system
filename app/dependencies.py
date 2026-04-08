b0d84f1566525d33e2c3f65b658393702eda738b
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-08 17:41:27 +0300

    create get_async_client dependency

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,5 +1,6 @@
 from typing import TYPE_CHECKING, Annotated
 
+import httpx
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -27,3 +28,11 @@ async def get_uow(
 
 
 UOWDep = Annotated[UnitOfWork, Depends(get_uow)]
+
+
+async def get_async_client() -> AsyncGenerator[httpx.AsyncClient, Any]:
+    async with httpx.AsyncClient() as client:
+        yield client
+
+
+AsyncClientDep = Annotated[httpx.AsyncClient, Depends(get_async_client)]
