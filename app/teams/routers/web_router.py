0d66d215a3fa279aa2f2e1bf73def23184967d7c
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-09 15:42:30 +0300

    add team add member page

--- app/teams/routers/web_router.py
+++ app/teams/routers/web_router.py
@@ -2,11 +2,13 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, Form, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi_users import exceptions
+from pydantic import EmailStr
 
 from app.auth.fastapi_users_instance import current_active_user, get_current_admin
 from app.auth.models import User
-from app.teams.dependencies import TeamServiceDep, team_of_current_user
-from app.teams.exceptions import TeamAlreadyExistsError
+from app.teams.dependencies import TeamServiceDep, current_team, team_of_current_user
+from app.teams.exceptions import TeamAlreadyExistsError, UserAlreadyInTeamError
 from app.teams.models import Team
 from app.teams.schemas import TeamCreate
 from app.templates import templates
@@ -17,6 +19,8 @@ TEAMS_LIST_PAGE_ROUTE_NAME = "teams:list_page"
 TEAMS_CREATE_PAGE_ROUTE_NAME = "teams:create_page"
 TEAMS_CREATE_ROUTE_NAME = "create_team"
 TEAMS_DETAIL_PAGE_ROUTE_NAME = "teams:detail_page"
+TEAMS_ADD_MEMBER_PAGE_ROUTE_NAME = "teams:add_member_page"
+TEAMS_ADD_MEMBER_ROUTE_NAME = "add_team_member"
 
 
 @router.get("/", response_class=HTMLResponse, name=TEAMS_LIST_PAGE_ROUTE_NAME)
@@ -99,5 +103,61 @@ async def team_detail_page(
             "team": team,
             "members": members,
             "tasks": tasks,
+        },
+    )
+
+
+@router.get(
+    "/{team_id}/add_member",
+    response_class=HTMLResponse,
+    name=TEAMS_ADD_MEMBER_PAGE_ROUTE_NAME,
+)
+async def team_add_member_page(
+    team: Annotated[Team, Depends(current_team)],
+    user: Annotated[User, Depends(get_current_admin)],
+    request: Request,
+):
+    return templates.TemplateResponse(
+        request=request,
+        name="teams/add_member.html",
+        context={
+            "title": "Add member",
+            "current_user": user,
+            "team": team,
+        },
+    )
+
+
+@router.post(
+    "/{team_id}/members", response_class=HTMLResponse, name=TEAMS_ADD_MEMBER_ROUTE_NAME
+)
+async def team_add_member(
+    team: Annotated[Team, Depends(current_team)],
+    user_email: Annotated[EmailStr, Form()],
+    user: Annotated[User, Depends(get_current_admin)],
+    teams_service: TeamServiceDep,
+    request: Request,
+):
+    try:
+        await teams_service.add_user_to_team(team, user_email)
+    except exceptions.UserNotExists:
+        error = "User with this email does not exist"
+    except UserAlreadyInTeamError:
+        error = "User with this email already in team"
+    else:
+        return RedirectResponse(
+            request.url_for(TEAMS_DETAIL_PAGE_ROUTE_NAME, team_id=team.id),
+            status_code=status.HTTP_303_SEE_OTHER,
+        )
+
+    return templates.TemplateResponse(
+        request=request,
+        name="teams/add_member.html",
+        context={
+            "title": "Add member",
+            "current_user": user,
+            "team": team,
+            "user_email": user_email,
+            "error": error,
         },
     )
