35012bb219ecc0f2aa3d528901d38fdd479d718d
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-09 15:19:10 +0300

    add team details page

--- app/teams/routers/web_router.py
+++ app/teams/routers/web_router.py
@@ -5,8 +5,9 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 
 from app.auth.fastapi_users_instance import current_active_user, get_current_admin
 from app.auth.models import User
-from app.teams.dependencies import TeamServiceDep
+from app.teams.dependencies import TeamServiceDep, team_of_current_user
 from app.teams.exceptions import TeamAlreadyExistsError
+from app.teams.models import Team
 from app.teams.schemas import TeamCreate
 from app.templates import templates
 
@@ -15,6 +16,7 @@ router = APIRouter(prefix="/teams")
 TEAMS_LIST_PAGE_ROUTE_NAME = "teams:list_page"
 TEAMS_CREATE_PAGE_ROUTE_NAME = "teams:create_page"
 TEAMS_CREATE_ROUTE_NAME = "create_team"
+TEAMS_DETAIL_PAGE_ROUTE_NAME = "teams:detail_page"
 
 
 @router.get("/", response_class=HTMLResponse, name=TEAMS_LIST_PAGE_ROUTE_NAME)
@@ -72,5 +74,30 @@ async def create_team(
             "current_user": user,
             "team_data": team_data,
             "error": error,
+        },
+    )
+
+
+@router.get(
+    "/{team_id}", response_class=HTMLResponse, name=TEAMS_DETAIL_PAGE_ROUTE_NAME
+)
+async def team_detail_page(
+    team: Annotated[Team, Depends(team_of_current_user(current_active_user))],
+    user: Annotated[User, Depends(current_active_user)],
+    teams_service: TeamServiceDep,
+    request: Request,
+):
+    members = await teams_service.get_team_members(team)
+    tasks = await teams_service.get_team_tasks(team)
+
+    return templates.TemplateResponse(
+        request=request,
+        name="teams/detail.html",
+        context={
+            "title": f"Team {team.name}",
+            "current_user": user,
+            "team": team,
+            "members": members,
+            "tasks": tasks,
         },
     )
