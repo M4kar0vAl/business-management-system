c7dbf49816ee89cf2b224f3d8851fac3fe9cbd07
Alexey Makarov <alex.makarov2290@gmail.com>
2026-04-11 16:32:14 +0300

    add meetings detail page

--- app/meetings/routers/web_router.py
+++ app/meetings/routers/web_router.py
@@ -4,13 +4,15 @@ from fastapi import APIRouter, Depends, Query, Request
 
 from app.auth.fastapi_users_instance import current_active_user
 from app.auth.models import User
-from app.meetings.dependencies import MeetingServiceDep
+from app.meetings.dependencies import MeetingServiceDep, current_meeting
+from app.meetings.models import Meeting
 from app.meetings.schemas import MeetingFilters
 from app.templates import templates
 
 router = APIRouter(prefix="/meetings")
 
 MEETING_LIST_PAGE_ROUTE_NAME = "meetings:list_page"
+MEETING_DETAIL_PAGE_ROUTE_NAME = "meetings:detail_page"
 
 
 @router.get("/", name=MEETING_LIST_PAGE_ROUTE_NAME)
@@ -30,5 +32,24 @@ async def meetings_list_page(
             "current_user": user,
             "meetings": meetings,
             "filters": filters,
+        },
+    )
+
+
+@router.get("/{meeting_id}", name=MEETING_DETAIL_PAGE_ROUTE_NAME)
+async def meetings_detail_page(
+    meeting: Annotated[
+        Meeting, Depends(current_meeting(current_active_user, full=True))
+    ],
+    user: Annotated[User, Depends(current_active_user)],
+    request: Request,
+):
+    return templates.TemplateResponse(
+        request=request,
+        name="meetings/detail.html",
+        context={
+            "title": f"Meeting {meeting.id}",
+            "current_user": user,
+            "meeting": meeting,
         },
     )
